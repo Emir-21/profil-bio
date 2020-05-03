b7fdb95e6cdf34a46d418ac3b103f11f1f74454d
Emir-21 <emirhez@gmail.com>
2020-05-03 22:09:56 +0700

    file asli

---
+++ MyAppProf/app/src/main/java/com/example/myapplication/Main2Activity.java
@@ -0,0 +1,26 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+
+    public void kembali(View view) {
+        Intent intent = new Intent(Main2Activity.this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void next(View view) {
+        Intent intent = new Intent(Main2Activity.this, Main3Activity.class);
+        startActivity(intent);
+    }
+}
---
+++ MyAppProf/app/src/main/java/com/example/myapplication/Main3Activity.java
@@ -0,0 +1,21 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Main3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+    }
+
+    public void home(View view) {
+        Intent intent = new Intent(Main3Activity.this, MainActivity.class);
+        startActivity(intent);
+    }
+}
